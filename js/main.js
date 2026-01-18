4f06001889a7a46e39f06b40cfd38169cfb01ee8
NicoLopezTec <nlopez.developer@outlook.com>
2026-01-18 14:21:47 -0300

    Corrige rutas de assets y carga del logo en GitHub Pages, y agrego script JS en ruta js.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const navMenu = document.getElementById('nav-menu');
+
+hamburgerBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('active'); // Muestra/oculta el menú
+    });
